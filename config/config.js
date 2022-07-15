68616082e52929b3cac16dc922d9ecb6664c5eb5
voloxatyj <ostapmedunskij@gmail.com>
2022-07-15 13:38:37 +0300

    Add API to configs

--- config/config.js
+++ config/config.js
@@ -8,4 +8,8 @@ config.app = process.env.APP || 'dev';
 config.host = process.env.HOST || 'localhost';
 config.port = process.env.PORT || '3000';
 
+config.cashInAPI = process.env.CASHINAPI;
+config.cashOutNaturalAPI = process.env.CASHOUTNATURALAPI;
+config.cashOutLegalAPI = process.env.CASHOUTLEGALAPI;
+
 export default config;
